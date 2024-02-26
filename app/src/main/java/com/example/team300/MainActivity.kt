a62c1bed7c336ab8bb9e63546d7495ff2944b4ef
Juseonghyun <wntjdgus99@naver.com>
2024-02-26 17:06:32 +0900

    - imageView, Activitiy 각 번호 1 = 김찬교 2 = 주성현 3 = 배기정 4 = 이희광
    - AndroidManifest
    Splash로 Main 변경햇음
    
    - SplashActivity
    Handler 추가, 3초뒤 MainActivity실행하게 했음
    
    - Gradle mudule:app
    viewBinding 추가했음
    
    - font 폴더추가
    배민 폰트로 변경했음
    
    - MainActivity
    뷰바인딩 사용했음

---
+++ app/src/main/java/com/example/team300/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.team300
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.team300.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // 뷰바인딩 관련 로직
+    private var mainBinding: ActivityMainBinding? = null
+    private val binding get() = mainBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        // 뷰바인딩 관련 로직
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.iv1.setOnClickListener {
+            val intent = Intent(this, ProfileActivity1::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.iv2.setOnClickListener{
+            val intent = Intent(this, ProfileActivity2::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.iv3.setOnClickListener {
+            val intent = Intent(this, ProfileActivity3::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.iv4.setOnClickListener {
+            val intent = Intent(this, ProfileActivity4::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/team300/ProfileActivity1.kt
@@ -0,0 +1,11 @@
+package com.example.team300
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile1)
+    }
+}
