61814a3771087e5dcc77f9b71c1cf6435df2852b
theBettor <be0128st@gmail.com>
2024-02-28 19:02:13 +0900

    2-2

--- app/src/main/java/com/example/team300/ProfileActivity1.kt
+++ app/src/main/java/com/example/team300/ProfileActivity1.kt
@@ -1,11 +1,26 @@
 package com.example.team300
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.example.team300.databinding.ActivityProfile1Binding
 class ProfileActivity1 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfile1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile1)
+        binding = ActivityProfile1Binding.inflate(layoutInflater).apply {
+            backButton.setOnClickListener { finish() }
+
+            cardviewAppcompatImageView.scaleY = -1f
+            linkedinImageButton.setOnClickListener {
+                var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/%EC%B0%AC%EA%B5%90-%EA%B9%80-3845a42a6/"))
+                startActivity(intent)
+            }
+
+
+            setContentView(root)
+        }
     }
 }
